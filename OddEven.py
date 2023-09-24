d26ad53e40b0e96d84c345aaa2cffe5cd5fad41f
unknown <devdattanarawade97@gmail.com>
2023-09-24 17:11:39 +0530

    problem

---
+++ OddEven.py
@@ -0,0 +1,18 @@
+
+class OddEven:
+
+
+    def findOddEven(number):
+
+        if number%2==0:
+
+            return "Even"
+        
+        else:
+
+            return "Odd"
+        
+    
+    print(findOddEven(5))
+        
+    
