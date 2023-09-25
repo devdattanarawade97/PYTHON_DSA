92b8e5abf8b4e0134a29506a984810c5d6287c89
unknown <devdattanarawade97@gmail.com>
2023-09-25 09:27:25 +0530

    problem

---
+++ checkPalindrome.py
@@ -0,0 +1,29 @@
+
+
+class CheckPalindrome:
+
+
+    def check():
+
+
+        givenString=input('enter string : ')
+
+        reversedString=givenString[::-1]
+
+
+        if(givenString==reversedString):
+
+            return True
+        
+        else:
+
+
+            return False
+        
+    
+    print(check())
+
+
+
+    
+
