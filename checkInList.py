8eaf55e390c4ee53a03dd67e76c93374d8fd52cf
unknown <devdattanarawade97@gmail.com>
2023-09-26 20:59:00 +0530

    problem

---
+++ checkInList.py
@@ -0,0 +1,22 @@
+
+class checkInList:
+
+
+
+    def check():
+
+        givenString=input('enter value : ')
+
+        listOfString=['hello', 'world', 'wise']
+
+
+        if givenString in listOfString:
+
+            print('present in list')
+
+        else:
+
+            print('not present in list')
+
+
+    check()
