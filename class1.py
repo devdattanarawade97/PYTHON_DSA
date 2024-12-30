399d3279ae9908af5cea844d47d5163dc2c55ae0
devdatta <devdattanarawade97@gmail.com>
2024-12-30 13:32:44 +0530

    updated

--- class1.py
+++ class1.py
@@ -4,10 +4,18 @@ class class1:
     def __init__(self):
         self.a=10
         self.b=20
+        self.__c=30
 
     def add(self):  
         return self.a+self.b
+    
+    def print_all_variables(self):
+        print("var a : ",self.a)
+        print("var b : ",self.b)
+        print("var c : ",self.__c)
 
 
 obj=class1()
 print(obj.add())
+print("var a :",obj.a)
+print("var c :",obj.__c)
